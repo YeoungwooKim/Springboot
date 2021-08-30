764b238be49a363867601d1fdaffb36a8016cbd8
wooyaStdy <=>
2021-08-30 16:20:39 +0900

    second logger aspect

--- src/main/java/board/aop/LoggerAspect.java
+++ src/main/java/board/aop/LoggerAspect.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 @Slf4j
-public class LoggerAspect {
+public class LoggerAspect {   
 	
 	@Around("execution(* board..controller.*Controller.*(..)) or execution(* board..service.*Impl.*(..)) or execution(* board..mapper.*Mapper.*(..))")
 	public Object logPrint(ProceedingJoinPoint joinPoint) throws Throwable {
