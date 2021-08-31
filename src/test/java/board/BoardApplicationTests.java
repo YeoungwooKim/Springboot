2c6e4217ea030f5f74cde0e12533e95146a6b65f
wooyaStdy <=>
2021-08-31 18:53:53 +0900

    testBuild err fixed, build complete!

--- src/test/java/board/BoardApplicationTests.java
+++ src/test/java/board/BoardApplicationTests.java
@@ -3,6 +3,8 @@ package board;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class BoardApplicationTests {
 
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
@@ -20,6 +23,7 @@ public class BoardApplicationTests {
 
 	@Test
 	public void testSqlSession() throws Exception{
+		log.debug(sqlSession.toString()+" hello");
 		System.out.println(sqlSession.toString());
 	}
 }
